4c5e6ac628be36c92ee17528b6ca1aa12aef528b
bestpisit <bestpisit@hotmail.com>
2024-07-10 15:18:27 +0700

    [update] fetching and normalize and format the data

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
-function tsvJSON(tsv) {
-    var lines = tsv.split("\n");
-    var result = [];
-    var headers = lines[0].split("\t");
-
-    for (var i = 1; i < lines.length; i++) {
-        var obj = {};
-        var currentline = lines[i].split("\t");
-
-        for (var j = 0; j < headers.length; j++) {
-            obj[headers[j]] = currentline[j];
-        }
-
-        result.push(obj);
-    }
-
-    return JSON.stringify(result);
-}
-
 async function fetchAndConvertTSVtoJSON(url) {
     try {
-        let response = await fetch(url);
-        let tsvText = await response.text();
+        const response = await fetch(url);
+        const tsvText = await response.text();
+        const [headerLine, ...lines] = tsvText.split('\n');
+        const headers = headerLine.split('\t').map(normalizeString);
 
-        const json = tsvJSON(tsvText);
+        const data = lines.map(line => {
+            const values = line.split('\t');
+            return headers.reduce((obj, header, index) => {
+                obj[header] = normalizeString(values[index]);
+                return obj;
+            }, {});
+        });
 
-        for(let record of json){
-            // for(let)
-        }
-        console.log(json[0])
-        console.log(json[0][Object.keys(json[0])[0]].toString('utf16le'));
+        return { keys: headers, json: data };
     } catch (error) {
         console.error('Error fetching or converting TSV:', error);
     }
 }
 
-let url = 'https://myweb.cmu.ac.th/sansanee.a/ISNE_MLDS/dataset/ionosphere.txt';
-fetchAndConvertTSVtoJSON(url);
+function normalizeString(input) {
+    if (!input) return '';
+
+    // Decode hexadecimal escape sequences
+    let normalString = input.replace(/\\x([0-9A-Fa-f]{2})/g, (_, p1) => String.fromCharCode(parseInt(p1, 16)));
+
+    // Remove null characters
+    normalString = normalString.replace(/\x00/g, '');
+
+    // Determine if the string is a float
+    if (isFloat(normalString)) {
+        return parseFloat(normalString);
+    } else {
+        return normalString;
+    }
+}
+
+function isFloat(str) {
+    return !isNaN(str) && !isNaN(parseFloat(str));
+}
+
+
+main();
+
+async function main() {
+    let url = 'https://myweb.cmu.ac.th/sansanee.a/ISNE_MLDS/dataset/ionosphere.txt';
+    var data = await fetchAndConvertTSVtoJSON(url);
+    console.log(data.keys, data.json[0]);
+}
