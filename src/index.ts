eb8bfd54df273f8e1dbf35091158179ef424c354
bestpisit <bestpisit@hotmail.com>
2024-08-01 14:46:52 +0700

    [update] cv with fold

--- src/index.ts
+++ src/index.ts
@@ -13,28 +13,52 @@ const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
 
 // GetData("https://myweb.cmu.ac.th/sansanee.a/ISNE_MLDS/dataset/ionosphere.txt");
 const mockData = {
-    keys: ['f1', 'f2'],
+    keys: ['f1', 'f2', 'f3'],
     json: [
-        { f1: 1, f2: 2, class: 'A' },
-        { f1: 3, f2: 4, class: 'A' },
-        { f1: 5, f2: 6, class: 'B' },
-        { f1: 3, f2: 4, class: 'B' }
+        { "f1": 1, "f2": 2, "f3": 0, class: 'A' },
+        { "f1": 3, "f2": 4, "f3": 0, class: 'A' },
+        { "f1": 5, "f2": 6, "f3": 0, class: 'B' },
+        { "f1": 3, "f2": 4, "f3": 0, class: 'B' },
+        { "f1": 1, "f2": 2, "f3": 0, class: 'A' },
+        { "f1": 3, "f2": 4, "f3": 0, class: 'A' },
+        { "f1": 5, "f2": 6, "f3": 0, class: 'B' },
+        { "f1": 3, "f2": 4, "f3": 0, class: 'B' },
+        { "f1": 3, "f2": 4, "f3": 0, class: 'B' },
+        { "f1": 3, "f2": 4, "f3": 0, class: 'B' },
     ]
 }
 
 let avg1 = 0, avg2 = 0;
-checkMain();
-async function checkMain() {
-    const n = 1;
+checkMain(data);
+async function checkMain(dataSet: any) {
+    const crossValidation = 0.1; // 10% of data for cross-validation
+    const n = 1/crossValidation;
     for (let i = 0; i < n; i++) {
-        await main(false);
+        const trainData: any = [];
+        const testData: any = [];
+        for(let j=0;j<dataSet.json.length;j++){
+            // console.log(j)
+            if(j>=i*crossValidation*dataSet.json.length && j < (i+1)*crossValidation*dataSet.json.length){
+                console.log(j);
+                testData.push(dataSet.json[j]);
+            }
+            else{
+                trainData.push(dataSet.json[j]);
+            }
+        }
+        // const shuffledData = data.json.sort(() => Math.random() - 0.5); //randomly shuffle data
+
+        // const trainSize = Math.floor(shuffledData.length * (1 - crossValidation));
+        // const trainData = shuffledData.slice(0, trainSize);
+        // const testData = shuffledData.slice(trainSize);
+        await main(false,dataSet,trainData,testData);
         // await main(true);
     }
-    // console.log("Accuracy 1: " + (avg1/n).toFixed(2) + "%");
-    // console.log("Accuracy 2: " + (avg2/n).toFixed(2) + "%");
+    console.log("Accuracy 1: " + (avg1 / n).toFixed(2) + "%");
+    console.log("Accuracy 2: " + (avg2 / n).toFixed(2) + "%");
 }
 
-async function main(check: boolean) {
+async function main(check: boolean, data: any, trainData: any, testData: any) {
     let { xi1, xi2 } = SFS(data); // Calculate best features
 
     if (check) {
@@ -43,13 +67,6 @@ async function main(check: boolean) {
     }
 
     console.log(`Best features: ${xi1}, ${xi2}`); // Output best features
-
-    const crossValidation = 0.1; // 10% of data for cross-validation
-    const shuffledData = data.json.sort(() => Math.random() - 0.5); //randomly shuffle data
-
-    const trainSize = Math.floor(shuffledData.length * (1 - crossValidation));
-    const trainData = shuffledData.slice(0, trainSize);
-    const testData = shuffledData.slice(trainSize);
 
 
     // NBC Data
@@ -66,10 +83,10 @@ async function main(check: boolean) {
     NBCML.train(NBCData);
 
     const confusionMatrix = ConfusionMatrix(
-        trainData.map((row: any) => (
+        testData.map((row: any) => (
             row['class']
         )),
-        trainData.map((row: any) => NBCML.predict([row[xi1], row[xi2]]))
+        testData.map((row: any) => NBCML.predict([row[xi1], row[xi2]]))
     );
 
     console.log(confusionMatrix);
@@ -95,28 +112,29 @@ async function main(check: boolean) {
         avg1 += f1;
     }
 
-    console.log("Accuracy: " + accuracy.toFixed(2) + "%");
-    console.log("Error Rate: " + errorRate.toFixed(2) + "%");
-    console.log("Sensitivity: " + sensitivity.toFixed(2) + "%");
-    console.log("Specificity: " + specificity.toFixed(2) + "%");
-    console.log("Precision: " + precision.toFixed(2) + "%");
-    console.log("Recall: " + recall.toFixed(2) + "%");
-    console.log("F1 Score: " + f1.toFixed(2) + "%");
+    // console.log("Accuracy: " + accuracy.toFixed(2) + "%");
+    // console.log("Error Rate: " + errorRate.toFixed(2) + "%");
+    // console.log("Sensitivity: " + sensitivity.toFixed(2) + "%");
+    // console.log("Specificity: " + specificity.toFixed(2) + "%");
+    // console.log("Precision: " + precision.toFixed(2) + "%");
+    // console.log("Recall: " + recall.toFixed(2) + "%");
+    // console.log("F1 Score: " + f1.toFixed(2) + "%");
 
 
-    const Values: any = [];
-    // bar1.start(data.json.length, 0);
+    // const Values: any = [];
+    // // bar1.start(data.json.length, 0);
 
-    for (const point of data.json) {
-        Values.push({ x: point[xi1], y: point[xi2], class: NBCML.predict([point[xi1], point[xi2]]) });
-        // bar1.update(Values.length);
-        // make it sleep for 1 second
-        // await new Promise(r => setTimeout(r, 10));
-    }
+    // for (const point of data.json) {
+    //     Values.push({ x: point[xi1], y: point[xi2], class: NBCML.predict([point[xi1], point[xi2]]) });
+    //     // Values.push({ x: point[xi1], y: point[xi2], class: point.class });
+    //     // bar1.update(Values.length);
+    //     // make it sleep for 1 second
+    //     // await new Promise(r => setTimeout(r, 10));
+    // }
 
-    fs.writeFileSync(path.resolve(__dirname, '../assets/Values.json'), JSON.stringify(Values));
+    // fs.writeFileSync(path.resolve(__dirname, '../assets/Values.json'), JSON.stringify(Values));
 
-    // bar1.stop(); // Stop the progress bar
+    // // bar1.stop(); // Stop the progress bar
 }
 
 // const distinctFeatures = new Set<string>();
